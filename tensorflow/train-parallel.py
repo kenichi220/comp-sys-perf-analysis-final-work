c60fcf61109990fa2b3d94c7bf18aaae3af619a2
Kenichi <kenichibrumati.k@gmail.com>
2025-10-04 23:20:29 -0300

    .

--- tensorflow/train-parallel.py
+++ tensorflow/train-parallel.py
@@ -159,37 +159,37 @@ test_dataset = (
     .batch(GLOBAL_BATCH_SIZE)
     .prefetch(tf.data.AUTOTUNE)
 )
-with tf.device('/GPU:0'):
-    with strategy.scope():
-        input_shape = (224, 224, 3)
-        base_model = ResNet50(weights=None, include_top=False, input_shape=input_shape)
-        base_model.trainable = True
 
-        x = GlobalAveragePooling2D()(base_model.output)
-        x = Dense(1024, activation="relu")(x)
-        predictions = Dense(num_classes, activation="softmax")(x)
+with strategy.scope():
+    input_shape = (224, 224, 3)
+    base_model = ResNet50(weights=None, include_top=False, input_shape=input_shape)
+    base_model.trainable = True
 
-        model = Model(inputs=base_model.input, outputs=predictions)
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = Dense(1024, activation="relu")(x)
+    predictions = Dense(num_classes, activation="softmax")(x)
 
-        BASE_LEARNING_RATE = 0.01
-        SCALED_LEARNING_RATE = BASE_LEARNING_RATE * NUM_WORKERS
+    model = Model(inputs=base_model.input, outputs=predictions)
 
-        opt = SGD(learning_rate=SCALED_LEARNING_RATE, weight_decay=0.0001, momentum=0.9)
+    BASE_LEARNING_RATE = 0.01
+    SCALED_LEARNING_RATE = BASE_LEARNING_RATE * NUM_WORKERS
 
-        model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+    opt = SGD(learning_rate=SCALED_LEARNING_RATE, weight_decay=0.0001, momentum=0.9)
+
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
     
-        take_gpu_snapshot(unique_id,log_directory)
-        monitor_processes,log_files = [],[]
-    try:
-        print("Iniciando o treinamento distribuído...")
-        history = model.fit(train_dataset, epochs=5, validation_data=test_dataset)
+    take_gpu_snapshot(unique_id,log_directory)
+    monitor_processes,log_files = [],[]
+try:
+    print("Iniciando o treinamento distribuído...")
+    history = model.fit(train_dataset, epochs=5, validation_data=test_dataset)
 
-        if worker_id == 0:
-            print("\nIniciando avaliação final no worker 0...")
-            score = model.evaluate(test_dataset, verbose=1)
-            print(f"Loss (perda) no teste: {score[0]:.4f}")
-            print(f"Accuracy (acurácia) no teste: {score[1]:.4f}")
-    finally:
-        stop_continuous_monitoring(monitor_processes,log_files)
+    if worker_id == 0:
+        print("\nIniciando avaliação final no worker 0...")
+        score = model.evaluate(test_dataset, verbose=1)
+        print(f"Loss (perda) no teste: {score[0]:.4f}")
+        print(f"Accuracy (acurácia) no teste: {score[1]:.4f}")
+finally:
+    stop_continuous_monitoring(monitor_processes,log_files)
 
 print(f"\nWorker {worker_id} concluiu.")
