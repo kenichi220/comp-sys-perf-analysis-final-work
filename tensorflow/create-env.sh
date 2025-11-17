1af8db586a975607c65a18dc41f76eb76951ba8e
Rayan Raddatz <rayandematos@gmail.com>
2025-11-17 07:50:54 -0300

    starting decomposing to focus on optimizing the synchronous training

--- tensorflow/create-env.sh
+++ tensorflow/create-env.sh
@@ -2,7 +2,7 @@
 python3 -m venv venv
 
 . venv/bin/activate
-which python3
+which python
 
 python3 --version
 pip --version
@@ -11,7 +11,9 @@ pip install --upgrade pip
 pip install tensorflow==2.15.1
 pip install tensorflow[and-gpu]==2.15.1
 pip install tensorflow[and-cuda]==2.15.1
+pip install pandas # By default already installed, but need for poti for an unknow reason
 
 pip freeze > reqs.txt
 
 python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
+cat reqs.txt
---
+++ tensorflow/train-sync.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import SGD
+import os
+import json
+import time
+from datetime import datetime
+import pathlib
+import pandas as pd
+
+log_dir = "logs/profile/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir,
+    histogram_freq=1,
+    profile_batch='2,10'  # Profile batches 100 through 105
+)
+
+strategy = tf.distribute.MultiWorkerMirroredStrategy()
+tf_config_str = os.environ.get('TF_CONFIG', '{}')
+tf_config = json.loads(tf_config_str)
+task_info = tf_config.get('task', {})
+worker_id = task_info.get('index', 0)
+
+SEED = 1
+IMG_SIZE = (224, 224)
+EPOCHS = 5
+BATCH_SIZE_PER_REPLICA = 96
+NUM_CLASSES = 200
+NUM_TRAIN_IMAGES = 100000
+
+tf.random.set_seed(SEED)
+
+NUM_WORKERS = strategy.num_replicas_in_sync
+GLOBAL_BATCH_SIZE = BATCH_SIZE_PER_REPLICA * NUM_WORKERS
+
+print(f"Worker ID: {worker_id}")
+print(f"Número total de workers (réplicas): {NUM_WORKERS}")
+print(f"Batch size por worker: {BATCH_SIZE_PER_REPLICA}")
+print(f"Batch size global (total): {GLOBAL_BATCH_SIZE}\n")
+
+def load_tiny_imagenet_datasets(data_path, num_classes):
+    train_path = data_path / 'train'
+    val_path = data_path / 'val'
+
+    train_dataset = tf.keras.utils.image_dataset_from_directory(
+        train_path,
+        labels='inferred',
+        label_mode='categorical',
+        image_size=(64, 64),
+        interpolation='nearest',
+        batch_size=None,
+        shuffle=False
+    )
+
+    val_annotations_path = val_path / 'val_annotations.txt'
+    val_data = pd.read_csv(val_annotations_path, sep='\t', header=None, names=['File', 'Class', 'X1', 'Y1', 'X2', 'Y2'])
+
+    class_names = sorted(os.listdir(train_path))
+    class_to_idx = {name: index for index, name in enumerate(class_names)}
+
+    val_images = [str(val_path / 'images' / fname) for fname in val_data['File']]
+    val_labels_str = val_data['Class'].values
+    val_labels_int = [class_to_idx[name] for name in val_labels_str]
+    val_labels_cat = to_categorical(val_labels_int, num_classes=num_classes)
+
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_images, val_labels_cat))
+
+    def parse_image(filename, label):
+        image = tf.io.read_file(filename)
+        image = tf.image.decode_jpeg(image, channels=3)
+        return image, label
+
+    val_dataset = val_dataset.map(parse_image, num_parallel_calls=tf.data.AUTOTUNE)
+    return train_dataset, val_dataset
+
+
+def preprocess_image(image, label):
+    image = tf.cast(image, tf.float32) / 255.0
+    image = tf.image.resize(image, IMG_SIZE)
+    return image, label
+
+
+print("Carregando o dataset Tiny ImageNet de um diretório local...")
+tiny_imagenet_path = pathlib.Path('/scratch/rrdmatos/tiny-imagenet-200') # Set here the path to the dataset
+train_dataset, test_dataset = load_tiny_imagenet_datasets(tiny_imagenet_path, NUM_CLASSES)
+
+options = tf.data.Options()
+options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+
+train_dataset = train_dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = train_dataset.with_options(options)
+train_dataset = train_dataset.shuffle(NUM_TRAIN_IMAGES, seed=SEED)
+train_dataset = train_dataset.batch(GLOBAL_BATCH_SIZE)
+train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+train_dataset = train_dataset.cache()
+
+test_dataset = test_dataset.map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
+test_dataset = test_dataset.cache()
+test_dataset = test_dataset.with_options(options)
+test_dataset = test_dataset.batch(GLOBAL_BATCH_SIZE).prefetch(tf.data.AUTOTUNE)
+
+with strategy.scope():
+    input_shape = (224, 224, 3)
+    base_model = ResNet50(weights=None, include_top=False, input_shape=input_shape)
+    base_model.trainable = True
+
+    x = GlobalAveragePooling2D()(base_model.output)
+    x = Dense(1024, activation="relu")(x)
+    predictions = Dense(NUM_CLASSES, activation="softmax")(x)
+
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    BASE_LEARNING_RATE = 0.01
+    SCALED_LEARNING_RATE = BASE_LEARNING_RATE * NUM_WORKERS
+
+    opt = SGD(learning_rate=SCALED_LEARNING_RATE, weight_decay=0.0001, momentum=0.9)
+
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=["accuracy"])
+
+duration_seconds = 0
+print("Iniciando o treinamento distribuído...")
+
+start_train_time = time.perf_counter()
+history = model.fit(train_dataset, epochs=EPOCHS, validation_data=test_dataset, callbacks=[tensorboard_callback])
+close_train_time = time.perf_counter()
+
+duration_seconds = close_train_time - start_train_time
+
+score = model.evaluate(test_dataset, verbose=(1 if worker_id == 0 else 0))
+if worker_id == 0:
+    print("\nIniciando avaliação final no worker 0...")
+    print(f"Loss (perda) no teste: {score[0]:.4f}")
+    print(f"Accuracy (acurácia) no teste: {score[1]:.4f}")
+
+print("TIME TRAINING:", duration_seconds)
+print(f"\nWorker {worker_id} concluiu.")
